052bf31a2817e3ae3d755fa88c271095b6a09231
bobrotopor <kaster114@gmail.com>
2024-05-25 18:31:26 +0300

    исправил + добавил отрисовку времени

--- trajectory_gen/trajectory_gen.py
+++ trajectory_gen/trajectory_gen.py
@@ -1,5 +1,6 @@
 import numpy as np
 from numpy.typing import NDArray
+from matplotlib import pyplot as plt
 
 
 class TrajGenGPR(object):
@@ -17,7 +18,7 @@ class TrajGenGPR(object):
     def gen_gpr_scanning_traj(self, points: NDArray, line_types: list, gpr_flags: list):
         
         is_first_iter = True
-        scan_traj = np.empty((0,3)) # time/x/y/gpr_flags
+        scan_traj = np.empty((0,4)) # time/x/y/gpr_flags
         line_types = ['pass'] + line_types
         gpr_flags = [0] + gpr_flags
         
@@ -35,12 +36,11 @@ class TrajGenGPR(object):
             if line_types[idx] == 'curve':
                 continue
 
-            # n = section_points.shape[0]
-            # g_flags = gpr_flags[idx] + np.zeros((n,1))
-            # part_tr
-            scan_traj = np.vstack((scan_traj, section_points))
-            print(f'iter={idx}')
-            print(scan_traj)
+            n = section_points.shape[0]
+            g_flags = gpr_flags[idx] + np.zeros((n,1))
+            part_traj = np.hstack((section_points, g_flags))
+            scan_traj = np.vstack((scan_traj, part_traj))
+
 
         return scan_traj
 
@@ -49,13 +49,14 @@ class TrajGenGPR(object):
     def gen_line(self, p1: NDArray, p2: NDArray, init_time: float) -> NDArray:
         """Сгенерировать отрезок прямой на плоскости между двумя опорными точками."""
         step =  self.scan_vel * self.dt
-        line_len = np.sqrt((p2[0] - p1[0])**2 + (p2[1] - p1[1])**2) 
-        time = line_len / self.scan_vel
+        len_line = np.sqrt((p2[0] - p1[0])**2 + (p2[1] - p1[1])**2) 
+        dir_vec = np.array([p2[0] - p1[0], p2[1] - p1[1]])
+        dir_vec *= 1 / len_line
 
-        n = int(np.ceil(line_len / step))
-        t_arr = init_time + np.linspace(0, time, n)
-        x_arr = p1[0] + np.linspace(0, p2[0], n)
-        y_arr = p1[1] + np.linspace(0, p2[1], n)
+        n = int(np.ceil(len_line / step))
+        t_arr = init_time + np.array([ idx*self.dt for idx in range(1,n+1)])
+        x_arr = np.array([ p1[0] + idx*dir_vec[0]*step for idx in range(n)])
+        y_arr = np.array([ p1[1] + idx*dir_vec[1]*step for idx in range(n)])
 
         t_arr = t_arr.reshape((n,1))
         x_arr = x_arr.reshape((n,1))
@@ -65,12 +66,16 @@ class TrajGenGPR(object):
 
 
 if __name__ == '__main__':
-    tj = TrajGenGPR(dt=0.1, scan_vel=0.5)
+    tj = TrajGenGPR(dt=0.05, scan_vel=0.5)
 
-    points = np.array([[0,0],[0,1],[1,0.5],[0,0.5]])
-    l_types = ['line', 'curve', 'line']
+    points = np.array([[0,0],[0,1],[1,0.5],[1,-0.5]])
+    l_types = ['line', 'line', 'line']
     gpr_flags = [1,0,1]
-    tj.gen_gpr_scanning_traj(points=points, line_types=l_types, gpr_flags=gpr_flags)
-    # p1 = np.array([0,0])
-    # p2 = np.array([1,2])
-    # print(tj.gen_line(p1, p2, 10))
+    traj = tj.gen_gpr_scanning_traj(points=points, line_types=l_types, gpr_flags=gpr_flags)
+
+    print(traj)
+    plt.figure('Траектория')
+    plt.title('Траектория')
+    plt.scatter(traj[:, 1], traj[:,2], c=traj[:, 0])
+    plt.grid()
+    plt.show()
